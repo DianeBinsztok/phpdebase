6f06d269f299f47c418c0a2b9ee7e921ef4a3b68
Diane <diane.binsztok@le-campus-numerique.fr>
2022-01-21 15:43:30 +0100

    Ajout de variables  et

--- header.php
+++ header.php
@@ -6,7 +6,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mon CV</title>
+    <title><?= $metaTitle="Mon CV"; ?></title>
+    <meta name="description" content="<?=  $metaDescription="Consultez mon CV et contactez-moi"; ?>"/>
     <link rel="stylesheet" media="screen and (max-width : 800px)" href="./css/phonestyle.css">
     <link rel="stylesheet" media="screen and (min-width : 800px)" href="./css/style.css">
 
