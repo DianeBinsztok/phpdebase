c6c17ac7788587d2a0c36c6446518d1854dc54f4
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-01-24 13:56:00 +0100

    Page 404

--- error.php
+++ error.php
@@ -1,1 +1,6 @@
 <?php
+$metaTitle="Diane Binsztok - 404";
+$metaDescription = "Cette page n'existe pas";
+require "header.php";
+echo ("Désolé, la page que vous cherchez n'existe pas. Vous pouvez revenir à la <a href='./index.php/?page=accueil'>page d'accueil</a>");
+?>
