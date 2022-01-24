541cb6ae2ffe8032bed18ddcb08e7a4a13fecbca
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-01-24 15:16:09 +0100

    test : enregistrement de la valeur page de la requête GET dans une variable php

--- index.php
+++ index.php
@@ -3,17 +3,19 @@ $metaTitle="Diane Binsztok - Mon CV";
 $metaDescription = "Bienvenue sur la page d'accueil de mon CV";
 require "header.php";
 
-if(isset($_GET["page"])){
-if($_GET["page"]=="contact"){
-    include "contact.php";
-}elseif($_GET["page"]=="hobbies"){
-    include "hobbies.php";
-}else{
+if (isset($_GET["page"])){
+    $page=$_GET["page"];
+    if($page=="contact"){
+        include "contact.php";
+    }elseif($page=="hobbies"){
+        include "hobbies.php";
+    }else{
         include "error.php";
     }
 }else{
     include "accueil.php";
 }
+
 
 
 require "footer.php";
