167faf02036f2cacf22b4f9cb37be66b9723624a
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-01-24 15:54:41 +0100

    filtrage de la valeur  pour n'accepter que les String

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $metaDescription = "Bienvenue sur la page d'accueil de mon CV";
 require "header.php";
 
 if (isset($_GET["page"])){
-    $page=$_GET["page"];
+    $page= filter_input(INPUT_GET, 'page', FILTER_SANITIZE_STRING);
     if($page=="contact"){
         include "contact.php";
     }elseif($page=="hobbies"){
