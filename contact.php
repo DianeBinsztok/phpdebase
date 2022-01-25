faa760129ca467d49a48a9e8e8e822d316654c26
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-01-25 10:50:51 +0100

    Nouveau formulaire de contact

--- contact.php
+++ contact.php
@@ -1,7 +1,4 @@
-
-
-
-<!--les infos de tête-->
+<!--les infos d'en-tête-->
 <?php
 
 $metaTitle="Diane Binsztok - Contact";
@@ -13,60 +10,65 @@ $metaDescription = "Contactez-moi";
 
     <h2 class="titles_font">Me contacter</h2>
 
-    <form id="form_container" action="https://httpbin.org/get" method="post">
+    <form id="form_container" action="index.php?page=contact" method="post">
+
+        <div class="form_field">
+            <label for="civilite">Choisissez votre civilité:</label>
+            <select id="civilite" type="select">
+                <option value="civilite_titre">Civilité</option>
+                <option value="madame">Madame</option>
+                <option value="monsieur">Monsieur</option>
+                <option value="autre">Autre</option>
+            </select>
+        </div>
+
+        <div class="form_field">
+            <label for="nom">Votre nom:</label>
+            <input id="nom" type="text">
+        </div>
+
+        <div class="form_field">
+            <label for="prenom">Votre prénom:</label>
+            <input id="prenom" type="text">
+        </div>
+
+        <div class="form_field">
+            <label for="email">Votre email:</label>
+            <input id="email" type="email" placeholder="ex: jean@gmail.com">
+        </div>
 
         <div class="form_field">
             <div id="radio_form_container">
-                <label for="nom">Vous êtes:</label>
+                <label for="raison">La raison de votre prise de contact:</label>
                 <div id="radio_form_options">
                     <div class="option">
-                        <label for="private">un particulier:</label> <input type="radio" name="contact"
-                                                                            value="private">
+                        <label for="job">Proposition d'emploi</label> <input id="job" type="radio" name="raison_contact"
+                                                                            value="job_value">
                     </div>
                     <div class="option">
-                        <label for="company">une entreprise:</label> <input type="radio" name="contact"
-                                                                            value="company">
+                        <label for="info">Demande d'information et prestations</label> <input id="info" type="radio" name="raison_contact"
+                                                                            value="info_value">
+                    </div>
+                    <div class="option">
+                        <label for="other">Autre</label> <input id="other" type="radio" name="raison_contact"
+                                                                     value="other_value">
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="form_field">
-            <label for="objet">A quel sujet me contactez-vous?</label>
-            <select type="select">
-                <option value="coucou">choisir un sujet</option>
-                <option value="coucou">Me faire un coucou</option>
-                <option value="bisou">Me faire un bisou</option>
-                <option value="manger">Me faire à manger</option>
-            </select>
-        </div>
 
-        <div id="nom" class="form_field">
-            <label for="nom">Votre nom:</label>
-            <input type="text">
-        </div>
-
-        <div id="prenom" class="form_field">
-            <label for="prenom">Votre prénom:</label>
-            <input type="text">
-        </div>
-
-        <div id="email" class="form_field">
-            <label for="email">Votre email:</label>
-            <input type="email" required placeholder="ex: jean@gmail.com">
-        </div>
 
         <div id="message" class="form_field">
             <label for="message">Votre message:</label>
-            <textarea type="text"></textarea>
+            <textarea form="form_container"></textarea>
         </div>
+
         <div class="btn_container">
-            <input value="Envoyer(post)" type="submit">
+            <input value="Envoyer" type="submit" formmethod="post" formaction="index.php?page=contact">
         </div>
     </form>
 
 
 
 </main>
-
-
